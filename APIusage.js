6a1b2b1b337467eb0dd8141e0001d3e178e54a30
Dhokoh <davidhogarcia@gmail.com>
2022-10-31 16:16:44 -0500

    feat: modified some code within APIusage.js

--- APIusage.js
+++ APIusage.js
@@ -1,4 +1,3 @@
-
 //Defining the NASA API link and storing it
 let nasa_url_globalAPI = 'https://eonet.gsfc.nasa.gov/api/v3/events';
 let nasa_url_HurricaneRosslyn = 'https://eonet.gsfc.nasa.gov/api/v3/events/EONET_6285';
@@ -8,31 +7,22 @@ let chart_canvas1 = document.getElementById('chartf1');
 let chart_canvas2 = document.getElementById('chartf2');
 let chart_canvas3 = document.getElementById('chartf3');
 
-const create_labels_Y_axis = async () => {
+const readAPI_createLabels = async () => {
     label_array_y_axis = [];
-    const APIreader = await fetch(nasa_url_HurricaneRosslyn)
+    label_array_x_axis = [];
+    const APIreader = await fetch(nasa_url_HurricaneRosslyn);//faltabapunto y coma aqui equizde
     const data = await APIreader.json();
     console.log(data);
     let stormgeometry = data.geometry;
-    stormgeometry.forEach(elem_Y => {
-        console.log(elem_Y.magnitudeValue);
-        label_array_y_axis.push(elem_Y.magnitudeValue);
+    stormgeometry.forEach(elem_ => {
+        console.log(elem_.date);
+        label_array_x_axis.push(elem_.date);
+        console.log(label_array_x_axis);
+        console.log(elem_.magnitudeValue);
+        label_array_y_axis.push(elem_.magnitudeValue);
+        console.log(label_array_y_axis);
     });
     return label_array_y_axis;
 }
 
-const create_labels_X_axis = async () => {
-    label_array_x_axis = [];
-    const APIreader = await fetch(nasa_url_HurricaneRosslyn);
-    const data = await APIreader.json();
-    console.log(data);
-    let stormgeometry = data.geometry;
-    stormgeometry.forEach(elem_X => {
-        console.log(elem_X.date);
-        label_array_x_axis.push(elem_X);
-    })
-    return label_array_x_axis;
-}
-
-console.log(create_labels_X_axis());
-console.log(create_labels_Y_axis());
+readAPI_createLabels();
