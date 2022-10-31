def57bbe01652c5bf4f750ee0d68c6467cc9a42d
Dhokoh <davidhogarcia@gmail.com>
2022-10-31 16:49:41 -0500

    feat: chart renderized with hurricane Rosslyn data from NASA API

--- APIusage.js
+++ APIusage.js
@@ -1,3 +1,4 @@
+
 //Defining the NASA API link and storing it
 let nasa_url_globalAPI = 'https://eonet.gsfc.nasa.gov/api/v3/events';
 let nasa_url_HurricaneRosslyn = 'https://eonet.gsfc.nasa.gov/api/v3/events/EONET_6285';
@@ -7,7 +8,7 @@ let chart_canvas1 = document.getElementById('chartf1');
 let chart_canvas2 = document.getElementById('chartf2');
 let chart_canvas3 = document.getElementById('chartf3');
 
-const readAPI_createLabels = async () => {
+const create_chart_labels = async () => {
     label_array_y_axis = [];
     label_array_x_axis = [];
     const APIreader = await fetch(nasa_url_HurricaneRosslyn);//faltabapunto y coma aqui equizde
@@ -22,7 +23,34 @@ const readAPI_createLabels = async () => {
         label_array_y_axis.push(elem_.magnitudeValue);
         console.log(label_array_y_axis);
     });
-    return label_array_y_axis;
+    
+    //Defining new chart to render
+    let storm_geometry_chart = new Chart(chart_canvas1, {
+        type: 'line',
+        data: {
+            labels: label_array_x_axis,
+            datasets: [{
+                label: data.title,
+                data: label_array_y_axis,
+                backgroundColor: [
+                    'rbga(255, 80, 35, 0.2)',
+                    'rbga(67, 90, 55, 0.2)',
+                    'rbga(210, 73, 35, 0.2)',
+                    'rbga(175, 97, 125, 0.2)',
+                    'rbga(225, 80, 150, 0.2)',
+                    'rbga(134, 73, 55, 0.2)',
+                    'rbga(223, 180, 135, 0.2)',
+                    'rbga(67, 125, 135, 0.2)',
+                    'rbga(134, 180, 135, 0.2)',
+                    'rbga(200, 210, 79, 0.2)',
+                    'rbga(177, 67, 44, 0.2)',
+                    'rbga(120, 210, 235, 0.2)',
+                    'rbga(255, 165, 35, 0.2)',
+                    'rbga(201, 100, 99, 0.2)'
+                ]
+            }]
+        }
+    });
 }
 
-readAPI_createLabels();
+create_chart_labels();
