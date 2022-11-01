581d45e6c84e598a0814f16fd69196b907835797
David <davidhogarcia@gmail.com>
2022-10-31 22:58:07 -0500

    no big changes made

--- APIusage.js
+++ APIusage.js
@@ -8,49 +8,46 @@ let chart_canvas1 = document.getElementById('chartf1');
 let chart_canvas2 = document.getElementById('chartf2');
 let chart_canvas3 = document.getElementById('chartf3');
 
-const create_chart_labels = async () => {
+
+
+const render_chart = async () => {
     label_array_y_axis = [];
     label_array_x_axis = [];
-    const APIreader = await fetch(nasa_url_HurricaneRosslyn);//faltabapunto y coma aqui equizde
+    const APIreader = await fetch(nasa_url_HurricaneRosslyn);
     const data = await APIreader.json();
-    console.log(data);
     let stormgeometry = data.geometry;
     stormgeometry.forEach(elem_ => {
-        console.log(elem_.date);
         label_array_x_axis.push(elem_.date);
-        console.log(label_array_x_axis);
-        console.log(elem_.magnitudeValue);
         label_array_y_axis.push(elem_.magnitudeValue);
-        console.log(label_array_y_axis);
     });
     
     //Defining new chart to render
     let storm_geometry_chart = new Chart(chart_canvas1, {
-        type: 'line',
+        type: 'bar',
         data: {
             labels: label_array_x_axis,
             datasets: [{
                 label: data.title,
                 data: label_array_y_axis,
                 backgroundColor: [
-                    'rbga(255, 80, 35, 0.2)',
+                    'rbga(10, 80, 35, 0.2)',
                     'rbga(67, 90, 55, 0.2)',
-                    'rbga(210, 73, 35, 0.2)',
-                    'rbga(175, 97, 125, 0.2)',
-                    'rbga(225, 80, 150, 0.2)',
-                    'rbga(134, 73, 55, 0.2)',
-                    'rbga(223, 180, 135, 0.2)',
+                    'rbga(21, 73, 35, 0.2)',
+                    'rbga(15, 97, 125, 0.2)',
+                    'rbga(25, 80, 150, 0.2)',
+                    'rbga(34, 73, 55, 0.2)',
+                    'rbga(23, 180, 135, 0.2)',
                     'rbga(67, 125, 135, 0.2)',
-                    'rbga(134, 180, 135, 0.2)',
-                    'rbga(200, 210, 79, 0.2)',
-                    'rbga(177, 67, 44, 0.2)',
-                    'rbga(120, 210, 235, 0.2)',
-                    'rbga(255, 165, 35, 0.2)',
-                    'rbga(201, 100, 99, 0.2)'
+                    'rbga(34, 180, 135, 0.2)',
+                    'rbga(20, 210, 79, 0.2)',
+                    'rbga(77, 67, 44, 0.2)',
+                    'rbga(12, 210, 235, 0.2)',
+                    'rbga(55, 165, 35, 0.2)',
+                    'rbga(21, 100, 99, 0.2)'
                 ]
             }]
         }
     });
 }
 
-create_chart_labels();
+render_chart();
