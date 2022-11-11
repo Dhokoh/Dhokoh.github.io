1849c7cf40c5b59bba3a8c0ab896316410800c7f
FerPichardo <alexei_12pichardo@hotmail.com>
2022-11-11 17:37:13 -0600

    se realizaron cambios en el ccss de apiboard, con el cual se pretende añadir un grfico relacionado a los pokemon el html se tuvo que modificar debido al cambio en el css y al nuevo grafico de pokemon se genero el nuevo grafico y se realizo un contador de los pokemons que hay cambios en el index ageno al proyecto para geerar el grafico cambios en el css correspondiente al index de prueba

--- JS.js
+++ JS.js
@@ -1,3 +1,4 @@
+
 let nasa_url_globalAPI = 'https://eonet.gsfc.nasa.gov/api/v3/events';
 let nasa_url_HurricaneRosslyn = 'https://eonet.gsfc.nasa.gov/api/v3/events/EONET_6285';
 let url = 'https://pokeapi.co/api/v2/pokemon'
@@ -18,15 +19,27 @@ const apinasa = async() =>{
     });
 }
 
+const showLoading = () => {
+    var loader = document.getElementById("Loader")
+    loader.style.display = 'block'
+}
+
+const hideLoading = () => {
+    var loader = document.getElementById("Loader")
+    loader.style.display = 'none'
+}
+
 const pokeapi = async () => {
+    showLoading()
     const llamada = await fetch (url)
     const data = await llamada.json()
     const lista = []
-        for (let i = 1; i<= 3; i++){
+    const eje_x = []
+    const eje_y = []
+        for (let i = 1; i<= 905; i++){
         const llamada_dos = await fetch(url_dos+i)
         const data_dos = await llamada_dos.json()
         data_dos.types.forEach(e =>{
-            debugger
             if (lista.length === 0){
                 let objeto ={
                     tipo: e.type.name,
@@ -35,9 +48,13 @@ const pokeapi = async () => {
                 lista.push(objeto)
             }
             else{
-                if (lista.includes(e.type.name)){
-                    lista.cuenta += 1
-                } else{
+                const resultado = lista.find( (elemento,index) => {
+                    if(elemento.tipo === e.type.name){
+                        lista[index].cuenta +=1
+                    return true
+                    } 
+                })
+                if (!resultado) {
                     let objeto ={
                         tipo: e.type.name,
                         cuenta: 1
@@ -47,6 +64,113 @@ const pokeapi = async () => {
             }
         })
     }
-    console.log(lista)
+    lista.forEach( el => {
+        eje_x.push(el.tipo)
+        eje_y.push(el.cuenta)
+})
+
+    const datos = {
+        labels: eje_x,
+        datasets: [{
+            label: 'Tipos de Pokemon',
+            data: eje_y,
+                        backgroundColor: [
+                'rgba( 30, 132, 73,   0.5)',
+                'rgba(106, 90, 205,   0.5)',
+                'rgba(255, 0, 0,      0.5)',
+                'rgba( 174, 214, 241, 0.5)',
+                'rgba(0, 0, 255,      0.5)',
+                'rgba(88, 214, 141,   0.5)',
+                'rgba( 255, 255, 0 ,  0.5)',
+                'rgba(255, 159, 64,   0.5)',
+                'rgba(229, 152, 102,  0.5)',
+                'rgba(238, 130, 238,  0.5)',
+                'rgba(231, 76, 60,    0.5)',
+                'rgba(236, 112, 99,   0.5)',
+                'rgba( 211, 84, 0 ,   0.5)',
+                'rgba( 36, 113, 163 , 0.5)',
+                'rgba( 93, 173, 226 , 0.5)',
+                'rgba( 142, 68, 173 , 0.5)',
+                'rgba( 52, 152, 219 , 0.5)',
+                'rgba( 86, 101, 115 , 0.5)',
+            ],
+            borderColor: [
+                'rgba( 30, 132, 73,   1)',
+                'rgba(106, 90, 205,   1)',
+                'rgba(255, 0, 0,      1)',
+                'rgba( 174, 214, 241, 1)',
+                'rgba(0, 0, 255,      1)',
+                'rgba(88, 214, 141,   1)',
+                'rgba( 255, 255, 0 ,  1)',
+                'rgba(255, 159, 64,   1)',
+                'rgba(229, 152, 102,  1)',
+                'rgba(238, 130, 238,  1)',
+                'rgba(231, 76, 60,    1)',
+                'rgba(236, 112, 99,   1)',
+                'rgba( 211, 84, 0 ,   1)',
+                'rgba( 36, 113, 163 , 1)',
+                'rgba( 93, 173, 226 , 1)',
+                'rgba( 142, 68, 173 , 1)',
+                'rgba( 52, 152, 219 , 1)',
+                'rgba( 86, 101, 115 , 1)',
+            ],
+            borderWidth: 1
+        }]
+    }
+    const ctx = document.getElementById('myChart')
+    const myChart = new Chart(ctx,{
+        type: "bar",
+        data: datos,
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }}
+    )
+    const pas = document.getElementById('pastel')
+    const pastel = new Chart(pas,{
+        type: "pie",
+        data: datos,
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }}
+    )
+    hideLoading()
 }
+
+// const grafico = async () => {
+//     await fetch(pokeapi())
+//     const eje_x = []
+//     const eje_y = []
+//         lista.forEach( el => {
+//         eje_x.push(el.tipo)
+//         eje_y.push(el.cuenta)
+// })
+
+//     const datos = {
+//         labels: eje_x,
+//         datasets: [{
+//             label: 'Tipos de Pokemon',
+//             data: eje_y
+//         }]
+//     }
+//     const ctx = document.getElementById('myChart')
+//     const myChart = new Chart(ctx,{
+//         type: "bar",
+//         data: datos,
+//         options: {
+//             scales: {
+//                 y: {
+//                     beginAtZero: true
+//                 }
+//             }
+//         }}
+//     )
+// }
 pokeapi()
